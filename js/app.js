e853702801852eee83a19b9441c363783dfcc1b0
laddered <anotherladdertwist@gmail.com>
2018-09-06 15:08:51 +0300

    click-through handler

--- js/app.js
+++ js/app.js
@@ -1,29 +1,81 @@
 var my_news = [
     {
         author: 'Саша Печкин',
-        text: 'В четверг, четвертого числа...'
+        text: 'В четчерг, четвертого числа...',
+        bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
     },
     {
         author: 'Просто Вася',
-        text: 'Считаю, что $ должен стоить 35 рублей!'
+        text: 'Считаю, что $ должен стоить 35 рублей!',
+        bigText: 'А евро 42!'
     },
     {
         author: 'Гость',
-        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+        bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
     }
 ];
 
+var Article = React.createClass({
+    propTypes: {
+        data: React.PropTypes.shape({
+            author: React.PropTypes.string.isRequired,
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
+        })
+    },
+    getInitialState: function(){
+        return {
+            visible: false
+        };
+    },
+
+    readmoreClick: function(e){
+        e.preventDefault();
+        this.setState({visible: true})
+    },
+
+    render: function() {
+        var author = this.props.data.author,
+            text = this.props.data.text,
+            bigText = this.props.data.bigText,
+            visible = this.state.visible;
+        console.log('render',this); //добавили console.log
+        return (
+            <div className="article">
+                <p className ='news__author'>{author}:</p>
+                <p className ='news__text'>{text} </p>
+                <a href="#" onClick={this.readmoreClick} className={'news__readmore ' + (visible ? 'none' : '')}>
+                    Подробнее</a>
+                <p className={'news__bigText ' + (visible ? '' : 'none')}>{bigText}</p>
+            </div>
+        )
+    }
+});
+
 var News = React.createClass({
+    propTypes:{
+        data: React.PropTypes.array.isRequired
+    },
+
+    getInitialState: function(){
+        return {
+            counter: 0
+        }
+    },
+    onTotalNewsClick: function(){
+this.setState({counter: ++this.state.counter});
+    },
+
     render: function() {
         var data = this.props.data;
         var newsTemplate;
 
-        if (data.length > 0){
+        if (data.length > 0) {
             newsTemplate = data.map(function(item, index) {
                 return (
                     <div key={index}>
-                        <p className="news__author">{item.author}:</p>
-                        <p className="news__text">{item.text}</p>
+                        <Article data={item}/>
                     </div>
                 )
             })
@@ -35,17 +87,10 @@ var News = React.createClass({
         return (
             <div className="news">
                 {newsTemplate}
-                <strong className={data.length > 0 ? "": "none"}>Всего новостей: {data.length} </strong>
-            </div>
-        );
-    }
-});
-
-var Comments = React.createClass({
-    render: function() {
-        return (
-            <div className="comments">
-                Новостей нет. Комментировать нечего
+                <strong
+                    className={'news__count ' + (data.length > 0 ? "": "none")} onClick={this.onTotalNewsClick}>
+                    Всего новостей: {data.length}
+                    </strong>
             </div>
         );
     }
@@ -55,9 +100,8 @@ var App = React.createClass({
     render: function() {
         return (
             <div className="app">
-                Всем привет, я компонент App! Я умею отображать новости.
+                <h3>Новости</h3>
                 <News data={my_news}/>
-                <Comments />
             </div>
         );
     }
